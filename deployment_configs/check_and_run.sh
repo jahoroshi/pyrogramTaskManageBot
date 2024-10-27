cd1cebc5727175655958b72e49b69c6ab404bcc5
jahoroshi4y <jahoroshi4y@gmail.com>
2024-10-27 22:48:32 +0300

    Added dep_configs

---
+++ deployment_configs/check_and_run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Define the port to be used for the server
+PORT=8003
+
+# Check if the specified port is already in use
+if lsof -i :$PORT > /dev/null; then
+    echo "Port $PORT is already in use. Killing all processes using the port."
+
+    # Kill all processes using the specified port
+    lsof -t -i :$PORT | xargs -r kill -9
+
+    # Wait for 2 seconds before proceeding
+    sleep 2
+fi
+
+# Execute Gunicorn with the specified configuration
+exec /home/ubuntu/.cache/pypoetry/virtualenvs/ankichat-IcZeuUhH-py3.12/bin/gunicorn \
+    --workers 1 \
+    --bind 0.0.0.0:8003 \
+    -c /home/ubuntu/taskbot/deployment_configs/gunicorn_conf.py \
+    -k uvicorn.workers.UvicornWorker \
+    app:app
---
+++ deployment_configs/gunicorn_conf.py
@@ -0,0 +1,6 @@
+bind = "0.0.0.0:8003"
+workers = 3
+user = "ubuntu"
+loglevel = "info"
+errorlog = "/home/ubuntu/taskbot/logs/gunicorn_error.log"
+accesslog = "/home/ubuntu/taskbot/logs/gunicorn_access.log"
